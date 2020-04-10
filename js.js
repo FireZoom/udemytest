6f9c80e2d5502288becca2f940a9bc5df24df6db
Adil <adil_tursunov@mail.ru>
2020-04-10 22:53:18 +0500

    added some code in js

---
+++ js.js
@@ -0,0 +1,11 @@
+alert('Hello, World!');
+
+confirm('Are you stupid?');
+
+let userNum = +prompt('Okay, I`m kidding you, my dear friend. Please write your lovely number');
+
+if(userNum > 0){
+    alert('Ну ты и дурак, ахахха')
+}else{
+    alert('Ну ты конченный...')
+}
